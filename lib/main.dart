a04f8046c73225cf922dce2430284d8a93810d2e
hridoykhan51 <hridoykhan6786@gmail.com>
2025-01-25 02:32:02 +0600

    portfolio start

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget {
+  const myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "hridoy",
+      home: ,
+    );
+  }
+}
